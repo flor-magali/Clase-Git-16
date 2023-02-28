a0a354c8a7f80690c5c81063a7bbc4e3ddbb27bb
Florencia Cammarata <florencia_magali@hotmail.com.ar>
2023-02-28 12:26:17 -0300

    agregué persona a modulo y la use en script

--- modulo.py
+++ modulo.py
@@ -3,3 +3,5 @@ def suma(a,b):
 
 def resta(a,b):
     return a-b
+def persona():
+    pass
--- script.py
+++ script.py
@@ -1,6 +1,8 @@
-from modulo import suma, resta
+from modulo import suma, resta, persona
 
 print('Hola mundo')
 
 print ('La suma de 1 + 2 = {suma(1,2)}')
 print ('La suma de 2 - 1 = {resta(1,2)}')
+
+persona1=persona()
